66be0283052ce60e7ac787fd8e33f7f87e4f9480
zhnlyn <zhoniulyngdoh@gmail.com>
2024-08-29 12:22:28 +0530

    changes

--- query_data.py
+++ query_data.py
@@ -1,9 +1,9 @@
 import argparse
-# from dataclasses import dataclass
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
+from langchain.vectorstores.chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+
+from get_embedding_function import get_embedding_function
 
 CHROMA_PATH = "chroma"
 
@@ -24,28 +24,29 @@ def main():
     parser.add_argument("query_text", type=str, help="The query text.")
     args = parser.parse_args()
     query_text = args.query_text
+    query_rag(query_text)
 
+
+def query_rag(query_text: str):
     # Prepare the DB.
-    embedding_function = OpenAIEmbeddings()
+    embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    if len(results) == 0 or results[0][1] < 0.7:
-        print(f"Unable to find matching results.")
-        return
+    results = db.similarity_search_with_score(query_text, k=5)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
+    # print(prompt)
 
-    model = ChatOpenAI()
-    response_text = model.predict(prompt)
+    model = Ollama(model="llama3.1")
+    response_text = model.invoke(prompt)
 
-    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
+    return response_text
 
 
 if __name__ == "__main__":
